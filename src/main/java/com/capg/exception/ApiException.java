46e8713f25c0b4e4a0d0d85d557a4713ad58fdde
MRAMYASR <97224141+MRAMYASR@users.noreply.github.com>
2022-02-03 19:07:45 +0530

    Update ApiException.java

--- src/main/java/com/capg/exception/ApiException.java
+++ src/main/java/com/capg/exception/ApiException.java
@@ -1,10 +1,1 @@
-package com.capg.exception;
 
-public class ApiException extends RuntimeException {
-	
-	private static final long serialVersionUID = 1L;
-
-	public ApiException(String message) {
-		super(message);
-	}
-}
