784e44f469e5127f505bb5b961bbe2b23ed8df77
MRAMYASR <97224141+MRAMYASR@users.noreply.github.com>
2022-02-03 19:13:27 +0530

    Update QuestionServiceImpl.java

--- src/main/java/com/capg/service/QuestionServiceImpl.java
+++ src/main/java/com/capg/service/QuestionServiceImpl.java
@@ -1,11 +1,9 @@
 package com.capg.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import com.capg.exception.QuestionNotFoundException;
 import com.capg.model.Question;
 import com.capg.repository.QuestionRepository;
@@ -33,27 +31,6 @@ public class QuestionServiceImpl implements QuestionService {
 				.orElseThrow(() -> new QuestionNotFoundException("Q_ID :: " + q_id + " Not Found"));
 		return ResponseEntity.ok().body(question);
 				
-	}
-
-	@Override
-	public ResponseEntity<Question> updateQuestionById(int q_id, Question question) throws QuestionNotFoundException {
-		Question newQuestion = questionRepository.findById(q_id)
-				.orElseThrow(() -> new QuestionNotFoundException("Q_ID:: " + q_id + " Not Found"));
-		newQuestion.setQ_name(question.getQ_name());
-		newQuestion.setOption_a(question.getOption_a());
-		newQuestion.setOption_b(question.getOption_b());
-		newQuestion.setOption_c(question.getOption_c());
-		newQuestion.setOption_d(question.getOption_d());
-		final Question updatedQuestion = questionRepository.save(newQuestion);
-		return ResponseEntity.ok().body(updatedQuestion);
-	}
-
-	@Override
-	public ResponseEntity<Question> deleteQuestionById(int q_id) throws QuestionNotFoundException {
-		Question question = questionRepository.findById(q_id)
-				.orElseThrow(() -> new QuestionNotFoundException("Q_ID :: " + q_id + " Not Found"));
-		questionRepository.deleteById(q_id);
-		return ResponseEntity.ok().body(question);
 	}
 
 }
