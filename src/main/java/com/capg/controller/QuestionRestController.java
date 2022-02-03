6ccbc426ca97d94f6efc64cc375c3cecf4d0b896
MRAMYASR <97224141+MRAMYASR@users.noreply.github.com>
2022-02-03 19:05:25 +0530

    Update QuestionRestController.java

--- src/main/java/com/capg/controller/QuestionRestController.java
+++ src/main/java/com/capg/controller/QuestionRestController.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,14 +27,11 @@ public class QuestionRestController {
 	@Autowired
 	private CourseService courseService;
 
-	
-
-	@PostMapping("question/addQuestion")
-	public Question addQuestion(@RequestBody Question question) {
-		Course courseResponse = courseService.findById(question.getCourse().getCourse_id());
-		question.setCourse(courseResponse);
-		return questionService.addQuestion(question);
-
+	@PostMapping("question/createQuestion")
+	public ResponseEntity<Question> createQuestion(@Validated @RequestBody Question ques) {
+		Question a = questionService.addQuestion(ques);
+		System.out.println(a);
+		return new ResponseEntity<Question>(a, HttpStatus.OK);
 	}
 
 	@GetMapping("/question")
